d84b7ba82156ed6668738f033bbe5053537e4196
wasin pansophon <wpansophon@gmail.com>
2019-07-04 19:35:57 +0700

    receive ...interface{}

--- README.md
+++ README.md
@@ -13,11 +13,9 @@ func main() {
 	p := pool.New(10)
 
 	for i := 1; i <= 1000; i++ {
-		p.Go(func(i int) func() {
-			return func() {
-				fmt.Println(i)
-			}
-		}(i))
+		p.Go(func(args []interface{}) {
+			fmt.Println(args[0])
+		}, i)
 	}
 
 	p.Wait()
--- pool.go
+++ pool.go
@@ -2,6 +2,8 @@ package pool
 
 import "sync"
 
+type fn func(args []interface{})
+
 // Pool config
 type Pool struct {
 	guard chan struct{}
@@ -17,21 +19,21 @@ func New(concurrent int) *Pool {
 }
 
 // Go execute func
-func (p *Pool) Go(fn func()) {
+func (p *Pool) Go(fn fn, args ...interface{}) {
 	p.guard <- struct{}{}
 	p.wg.Add(1)
-	go p.execute(fn)
+	go p.execute(fn, args)
 }
 
-func (p *Pool) execute(fn func()) {
+func (p *Pool) execute(fn fn, args []interface{}) {
 	defer p.wg.Done()
 	defer func() {
 		<-p.guard
 	}()
-	fn()
+	fn(args)
 }
 
-// Wait wait execute routine
+// Wait execute routine
 func (p *Pool) Wait() {
 	p.wg.Wait()
 }
